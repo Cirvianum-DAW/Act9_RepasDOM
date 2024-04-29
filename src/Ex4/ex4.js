661bc459f003cfa3817ed1eb013bd3946be27522
PABLO ROS PROFE <pros7@xtec.cat>
2024-04-29 14:34:00 +0200

    semicolons

--- src/Ex4/ex4.js
+++ src/Ex4/ex4.js
@@ -1,29 +1,28 @@
-const app = document.getElementById("list-app");
+const app = document.getElementById('list-app');
 
 // Create the necessary elements
-const list = document.createElement("ul");
+const list = document.createElement('ul');
 app.appendChild(list);
-const input = document.createElement("input");
-input.setAttribute("type", "text");
-const addButton = document.createElement("button");
-addButton.innerHTML = "Create New";
+const input = document.createElement('input');
+input.setAttribute('type', 'text');
+const addButton = document.createElement('button');
+addButton.innerHTML = 'Create New';
 
 // Add event listeners to the elements
-addButton.addEventListener("click", function() {
-    const newItem = document.createElement("LI");
-    newItem.innerHTML = input.value;
-    list.appendChild(newItem);
-    input.value = "";
+addButton.addEventListener('click', function () {
+  const newItem = document.createElement('LI');
+  newItem.innerHTML = input.value;
+  list.appendChild(newItem);
+  input.value = '';
 });
-
 
 // When checking a tag name, we use the uppercase version of the tag name
 
-list.addEventListener("click", function(event) {
-    if (event.target.tagName === "LI") {
-        const text = prompt("Enter new text:");
-        event.target.innerHTML = text;
-    }
+list.addEventListener('click', function (event) {
+  if (event.target.tagName === 'LI') {
+    const text = prompt('Enter new text:');
+    event.target.innerHTML = text;
+  }
 });
 
 app.appendChild(input);
--- src/Ex5/ex5.js
+++ src/Ex5/ex5.js
@@ -1,29 +1,29 @@
 const languages = [
-    {
-      name: 'JavaScript',
-      releaseDate: 1995,
-      fileExtension: '.js',
-      creator: 'Brendan Eich',
-    },
-    {
-      name: 'Java',
-      releaseDate: 1995,
-      fileExtension: '.java',
-      creator: 'James Gosling',
-    },
-    {
-      name: 'PHP',
-      releaseDate: 1995,
-      fileExtension: '.php',
-      creator: 'Rasmus Lerdorf',
-    },
-    {
-      name: 'C++',
-      releaseDate: 1985,
-      fileExtension: '.cpp',
-      creator: 'Bjarne Stroustrup',
-    },
-  ];
+  {
+    name: 'JavaScript',
+    releaseDate: 1995,
+    fileExtension: '.js',
+    creator: 'Brendan Eich',
+  },
+  {
+    name: 'Java',
+    releaseDate: 1995,
+    fileExtension: '.java',
+    creator: 'James Gosling',
+  },
+  {
+    name: 'PHP',
+    releaseDate: 1995,
+    fileExtension: '.php',
+    creator: 'Rasmus Lerdorf',
+  },
+  {
+    name: 'C++',
+    releaseDate: 1985,
+    fileExtension: '.cpp',
+    creator: 'Bjarne Stroustrup',
+  },
+];
 
 // Table structure in html
 // <table>
@@ -52,43 +52,41 @@ const languages = [
 //   </tbody>
 // </table>
 
-
 function tableCreator(dataArray, targetElement) {
-    // Anem a intentar-ho fent ús de funcions del DOM només. 
+  // Anem a intentar-ho fent ús de funcions del DOM només.
 
-    // Creem els elements
-    const table = document.createElement('table');
+  // Creem els elements
+  const table = document.createElement('table');
 
-    // Creem els elements de la capçalera
-    const trElement = document.createElement('tr');
-    for (const key in dataArray[0]) {
-        const thElement = document.createElement('th');
-        thElement.innerHTML = key;
-        trElement.appendChild(thElement);
-    }
-    // Afegim la capçalera a la taula
-    table.appendChild(trElement);
-    // Creem els elements del cos de la taula 
-    for (const element of dataArray){
-        // Creem l'element per cada nova fila
-        const trElement = document.createElement('tr');
-        for (const key in element) {    
-            // Creem l'element per cada nova valor de la columna
-            const tdElement = document.createElement('td');
-            // En cada iteració anem afegint el valor de cada propietat {name: 'PHP', releaseDate: 1995, fileExtension: '.php', creator: 'Rasmus Lerdorf'}
-            tdElement.innerHTML = element[key]
-            trElement.appendChild(tdElement);
-        }
-        // Afegim el cos a la taula
-        table.appendChild(trElement);
-    }
-
-    // Afegim la taula al document
-    targetElement.appendChild(table);  
+  // Creem els elements de la capçalera
+  const trElement = document.createElement('tr');
+  for (const key in dataArray[0]) {
+    const thElement = document.createElement('th');
+    thElement.innerHTML = key;
+    trElement.appendChild(thElement);
   }
-  
-  // Crida a la funció
-  tableCreator(languages, document.getElementById('table-container'));
+  // Afegim la capçalera a la taula
+  table.appendChild(trElement);
+  // Creem els elements del cos de la taula
+  for (const element of dataArray) {
+    // Creem l'element per cada nova fila
+    const trElement = document.createElement('tr');
+    for (const key in element) {
+      // Creem l'element per cada nova valor de la columna
+      const tdElement = document.createElement('td');
+      // En cada iteració anem afegint el valor de cada propietat {name: 'PHP', releaseDate: 1995, fileExtension: '.php', creator: 'Rasmus Lerdorf'}
+      tdElement.innerHTML = element[key];
+      trElement.appendChild(tdElement);
+    }
+    // Afegim el cos a la taula
+    table.appendChild(trElement);
+  }
 
-  // source: https://www.codeguage.com/courses/js/html-dom-building-tables-exercise
-  
+  // Afegim la taula al document
+  targetElement.appendChild(table);
+}
+
+// Crida a la funció
+tableCreator(languages, document.getElementById('table-container'));
+
+// source: https://www.codeguage.com/courses/js/html-dom-building-tables-exercise
